80d1f735c523142b81de46c5f7bf4017b37bbdf7
kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2017-08-19 11:09:34 +0000

    fix bug (json serializer)

--- dkf.py
+++ dkf.py
@@ -30,6 +30,12 @@ class NumPyArangeEncoder(json.JSONEncoder):
 	def default(self, obj):
 		if isinstance(obj, np.int64):
 			return int(obj)
+		if isinstance(obj, np.int32):
+			return int(obj)
+		if isinstance(obj, np.float32):
+			return float(obj)
+		if isinstance(obj, np.float64):
+			return float(obj)
 		if isinstance(obj, np.ndarray):
 			return obj.tolist() # or map(int, obj)
 		return json.JSONEncoder.default(self, obj)
