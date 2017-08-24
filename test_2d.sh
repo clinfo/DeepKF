67f8e883368cd4b696e19a45e61195905fa4cad8
kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2017-08-24 04:15:27 +0000

    update]

--- test_2d.sh
+++ test_2d.sh
@@ -7,6 +7,7 @@ python dkf.py --config config_train.json --hyperparam ./model_2d/hyparam.json tr
 python dkf.py --config hyopt/config_infer.json --hyperparam ./model_2d/hyparam.result.json --save-config ./model_2d/config_infer2.json infer
 
 python dkf.py --config config_train_2d.json --hyperparam model_2d/hyparam.result.json filter
+
 python attractor.py --config config_train_2d.json --hyperparam model_2d/hyparam.result.json field
 python attractor.py --config config_train_2d.json --hyperparam model_2d/hyparam.result.json infer
 python attractor.py --config config_train_2d.json --hyperparam model_2d/hyparam.result.json potential
@@ -14,5 +15,5 @@ python attractor.py --config config_train_2d.json --hyperparam model_2d/hyparam.
 python script/plot_p.py ./model_2d/config_infer2.json all 
 python script/plot.py model_2d/config_infer2.json all
 python script/plot_vec.py model_2d/config_infer2.json
-
 python script/plot_pot.py model_2d/config_infer2.json
+
