cd40962b31cc02e78bb8dcbf38ea4806ed1efc51
ryosuke kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2019-06-04 11:52:44 +0900

    add sample_cycle/experiments

--- sample_cycle/run_experiments.sh
+++ sample_cycle/run_experiments.sh
@@ -2,18 +2,18 @@ cd `dirname $0`
 cd ..
 
 echo "... generating configs"
-mkdir -p sample_synth/experiments/configs/
-sh sample_synth/experiments/gen_configs.sh
+mkdir -p sample_cycle/experiments/configs/
+sh sample_cycle/experiments/gen_configs.sh
 
 
 export  CUDA_VISIBLE_DEVICES=3
-cfg=sample_synth/config.json
-for f in `ls sample_synth/experiments/configs/config.*.json`
+cfg=sample_cycle/config.json
+for f in `ls sample_cycle/experiments/configs/config.*.json`
 do
 b=`basename ${f} .json`
 c=${b#config.}
 echo "...running:" $c
-h=sample_synth/experiments/model.${c}/hyparam.result.json
+h=sample_cycle/experiments/model.${c}/hyparam.result.json
 
 python dmm.py --config ${cfg} --hyperparam ${f} --save-config ${h} train,infer,filter,field
 
