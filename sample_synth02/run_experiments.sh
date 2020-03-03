9f3a1c92f025e0e21cf9194cd423fc4d8de1ce1e
ryosuke kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2020-03-03 20:53:57 +0900

    add experiments for synth02

---
+++ sample_synth02/run_experiments.sh
@@ -0,0 +1,27 @@
+cd `dirname $0`
+
+# data generator
+#python make.py
+#python make_config.py
+
+
+for f in `ls experiments/config/config_base*.json`
+do
+echo $f
+dmm train,infer,filter,field --config ${f} --hyperparam hyparam_base.json
+dmm-plot infer --config ${f}
+dmm-plot filter --config ${f}
+dmm-plot infer --config ${f}  --z_plot_type scatter --anim
+dmm-plot filter --config ${f}
+done
+
+for f in `ls experiments/config/config_pot*.json`
+do
+echo $f
+dmm train,infer,filter,field --config ${f} --hyperparam hyparam_pot.json
+dmm-plot infer --config ${f}
+dmm-plot filter --config ${f}
+dmm-plot infer --config ${f}  --z_plot_type scatter --anim
+dmm-plot filter --config ${f}
+done
+
---
+++ sample_synth02/run_pot.sh
@@ -0,0 +1,11 @@
+cd `dirname $0`
+
+# data generator
+#python make.py
+
+dmm train,infer,filter,field --config config_pot.json --hyperparam hyparam_pot.json
+dmm-plot infer --config config_pot.json
+dmm-plot filter --config config_pot.json
+dmm-plot infer --config config_pot.json  --z_plot_type scatter --anim
+dmm-plot filter --config config_pot.json
+
