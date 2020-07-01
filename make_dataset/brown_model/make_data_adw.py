3aac2980d31d83c648a50cc768b468a105c995ed
tanaka-hiroki1989 <rokitaniplus1@gmail.com>
2020-07-01 13:38:46 +0900

    modified brown_model data generation

--- make_dataset/brown_model/make_data_adw.py
+++ make_dataset/brown_model/make_data_adw.py
@@ -8,10 +8,13 @@ import data_generator
 ## generate 50000 frames and energy values
 datapoints = int(5e4)
 
-# print(datapoints) ## 50000
+#print(datapoints) ## 50000
 
 traj_whole = data_generator.get_asymmetric_double_well_data(datapoints)
+for i in range(len(traj_whole)):
+    print(i,traj_whole[i])
 # To fit the dataformat
+"""
 traj_whole = np.expand_dims(traj_whole, 1)
 traj_data_points, input_size = traj_whole.shape
 
@@ -28,15 +31,15 @@ print(traj2.shape)
 np.save("adw_traj1.npy", traj1)
 # np.save('assym_traj2.npy', traj2)
 
-
+"""
 x = np.linspace(-1, 5, 500)
-plt.figure(figsize=(6, 2))
+plt.figure(figsize=(6, 4))
 plt.ylim(-15, 10)
 plt.xlim(-1, 5)
 plt.plot(x, data_generator.asymmetric_double_well_energy(x), lw=2)
-plt.xlabel("Position x / a.u.", fontsize=16)
-plt.ylabel("Pot. energy / a.u.", fontsize=16)
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
-plt.show()
+plt.xlabel("Position x / a.u.", fontsize=6)
+plt.ylabel("Pot. energy / a.u.", fontsize=6)
+plt.xticks(fontsize=6)
+plt.yticks(fontsize=6)
+#plt.show()
 plt.savefig("adw_potential.png")
