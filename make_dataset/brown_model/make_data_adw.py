fb4e84e92e9780a2221dd348d09135f479a3bd40
tanaka-hiroki1989 <rokitaniplus1@gmail.com>
2020-07-02 18:12:13 +0900

    20200702

--- make_dataset/brown_model/make_data_adw.py
+++ make_dataset/brown_model/make_data_adw.py
@@ -11,9 +11,12 @@ datapoints = int(5e4)
 #print(datapoints) ## 50000
 
 traj_whole = data_generator.get_asymmetric_double_well_data(datapoints)
-for i in range(len(traj_whole)):
-    print(i,traj_whole[i])
+#for i in range(len(traj_whole)):
+#    print(i,traj_whole[i])
 # To fit the dataformat
+
+plt.hist(traj_whole, bins=16)
+plt.savefig("hist1.png")
 """
 traj_whole = np.expand_dims(traj_whole, 1)
 traj_data_points, input_size = traj_whole.shape
@@ -31,7 +34,7 @@ print(traj2.shape)
 np.save("adw_traj1.npy", traj1)
 # np.save('assym_traj2.npy', traj2)
 
-"""
+
 x = np.linspace(-1, 5, 500)
 plt.figure(figsize=(6, 4))
 plt.ylim(-15, 10)
@@ -43,3 +46,4 @@ plt.xticks(fontsize=6)
 plt.yticks(fontsize=6)
 #plt.show()
 plt.savefig("adw_potential.png")
+"""
