ba4f588f41abbe75a84fc42202b432fec29baac8
Hiroki Tanaka <rokitaniplus1@gmail.com>
2020-07-17 16:41:31 +0900

    20200717

--- sample_adw_gen/sample_adw_gen_train.sh
+++ sample_adw_gen/sample_adw_gen_train.sh
@@ -1,7 +1,7 @@
-for i in `seq 1 2`
+for i in `seq 1 10`
 do
 alpha=`echo ${i}| awk '{print $1*0.1}'`
-    for j in `seq 1 2`
+    for j in `seq 1 10`
     do
         beta=`echo ${j}| awk '{print $1*0.1}'`
         hyparam=hyparam_adw_gen_${i}_${j}.json
@@ -12,10 +12,9 @@ alpha=`echo ${i}| awk '{print $1*0.1}'`
         echo "\t \"beta\": ${beta}," >> ${hyparam}
         echo "\t \"result_path\": \"result_${i}_${j}\"" >> ${hyparam}
         echo "}" >> ${hyparam}
-        dmm train,test,filter --config config_adw_gen.json\
+        dmm train --config config_adw_gen.json\
          --hyperparam hyparam_adw_gen_${i}_${j}.json\
          --save-config config_adw_gen_${i}_${j}.result.json --gpu 0
-        dmm-plot train --config config_adw_gen_${i}_${j}.result.json
-        dmm-plot filter --config config_adw_gen_${i}_${j}.result.json 
+        dmm-plot train --config config_adw_gen_${i}_${j}.result.json --limit_all 5
     done
 done
