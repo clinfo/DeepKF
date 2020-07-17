c9db161f8b18fb3e824359a611fa6a3ba2e525a8
Hiroki Tanaka <rokitaniplus1@gmail.com>
2020-07-17 11:20:41 +0900

    loop shell

--- sample_adw_gen/run_sample_adw_gen.sh
+++ sample_adw_gen/run_sample_adw_gen.sh
@@ -1,9 +1,14 @@
-alpha=0.1
-beta=0.2
-hyparam=hyparam_adw_gen_${alpha}_${beta}.json
-rm ${json}
-echo "{" >> ${hyparam} 
-echo "\t \"alpha\": ${alpha}," >> ${hyparam}
-echo "\t \"beta\": ${beta}," >> ${hyparam}
-echo "\t \"result_path\": \"result_${alpha}_${beta}\"" >> ${hyparam}
-echo "}" >> ${hyparam}
+for alpha in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+do
+    for beta in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+    do
+        hyparam=hyparam_adw_gen_${alpha}_${beta}.json
+        rm ${hyparam}
+        touch ${hyparam}
+        echo "{" >> ${hyparam} 
+        echo "\t \"alpha\": ${alpha}," >> ${hyparam}
+        echo "\t \"beta\": ${beta}," >> ${hyparam}
+        echo "\t \"result_path\": \"result_${alpha}_${beta}\"" >> ${hyparam}
+        echo "}" >> ${hyparam}
+    done
+done
