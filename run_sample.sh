ec41040d16352361ed8faa40177456c169cf75d2
Hiroki Tanaka <tanaka.hiroki.82c@st.kyoto-u.ac.jp>
2020-08-06 02:31:00 +0900

    modified run_sample.sh (draft)

--- run_sample.sh
+++ run_sample.sh
@@ -1,9 +1,13 @@
 
-export  CUDA_VISIBLE_DEVICES=0
+#export  CUDA_VISIBLE_DEVICES=0
 
-python dmm.py --config sample/config.json --hyperparam sample/hyparam.json --save-config ./model/config.result.json train,test,filter,field
+#python dmm.py --config sample/config.json --hyperparam sample/hyparam.json --save-config ./model/config.result.json train,test,filter,field
+dmm train,test,filter,field --config sample/config.json\
+ --hyperparam sample/hyparam.json\
+ --save-config model/config.result.json --gpu 0
 
-python script/plot_p.py --config model/config.result.json --hyperparam model/hyparam.result.json all 
-python script/plot.py --config model/config.result.json --hyperparam model/hyparam.result.json all
-python script/plot_vec.py model/config.result.json all
+#python script/plot_p.py --config model/config.result.json --hyperparam model/hyparam.result.json all 
+#python script/plot.py --config model/config.result.json --hyperparam model/hyparam.result.json all
+dmm-plot infer --config model/config.result.json --hyperparam model/hyparam.result.json 
+#python script/plot_vec.py model/config.result.json all
 
