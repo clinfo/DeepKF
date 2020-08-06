02de6eac23f4330d119e1c832327d45b7752d651
Hiroki Tanaka <rokitaniplus1@gmail.com>
2020-08-07 08:26:50 +0900

    adw infer

--- sample_adw_gen/adw_gen.sh
+++ sample_adw_gen/adw_gen.sh
@@ -1,6 +1,8 @@
 k=$1
-for i in `seq 1 10` do
-   for j in `seq 1 10` do
+for i in `seq 1 10`
+do
+    for j in `seq 1 10`
+    do
         dmm train,test --config config_adw_gen.json\
         --hyperparam hyparams/hyparam_adw_gen_${i}_${j}_${k}.json\
         --save-config config_results/config_adw_gen_${i}_${j}_${k}.result.json --gpu ${k}
