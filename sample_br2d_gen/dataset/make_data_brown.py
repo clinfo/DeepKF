e55dff8847aa57cf8fd97c7f68f91c6fe937d70b
Hiroki Tanaka <tanaka.hiroki.82c@st.kyoto-u.ac.jp>
2020-08-06 19:33:15 +0900

    add files br2d_gen

--- sample_br2d_gen/dataset/make_data_brown.py
+++ sample_br2d_gen/dataset/make_data_brown.py
@@ -2,44 +2,20 @@ import sys
 import numpy as np
 import data_generator
 
-
 nstep = int(999999)
 
-
-x = data_generator.get_folding_model_data(nstep=nstep,\
+x_train = data_generator.get_folding_model_data(nstep=nstep,\
      rvec0=2.0 * (np.random.rand(2) - 0.5), kT=1., dt = 0.1)
+print(x_train)
+print(x_train.shape)
+traj_train = np.reshape(x_train, (-1, 1000, 2))
+print(traj_train.shape)
+np.save("data_train.npy", traj_train)
 
-#traj_whole = x
-#traj_data_points, input_size = traj_whole.shape
-
-print(x)
-print(x.shape)
-
-traj1 = np.reshape(x, (-1, 1000, 2))
-print(traj1.shape)
-np.save("data_train.npy', traj1)
-
-#stride = 10
-#r = np.linalg.norm(x, axis=-1)[::stride]
-#pot = np.zeros_like(r)
-#for i in range(r.shape[0]):
-#    pot[i] = data_generator.folding_model_energy(r[i], 3)
-
-
-#plt.plot(r[::stride], pot[::stride], '.')
-#plt.xlabel('Distance x / a.u.', fontsize = 16)
-#plt.ylabel('Pot. energy / a.u.', fontsize = 16)
-#plt.xticks(fontsize = 14)
-#plt.yticks(fontsize = 14);
-#plt.show()
-#plt.savefig('folding_energy.png')
-
-#print(np.random.rand(2).shape) #(2, )
-#print(np.zeros((2)).shape) #(2,)
-#
-#
-#rvec = np.zeros(shape=(datapoints+1, 2))
-#print(rvec.shape) #(1e6, 2)
-#print(rvec[0, :].shape) #(2,)
-#
-#
+x_test = data_generator.get_folding_model_data(nstep=nstep,\
+     rvec0=2.0 * (np.random.rand(2) - 0.5), kT=1., dt = 0.1)
+print(x_test)
+print(x_test.shape)
+traj_test = np.reshape(x_test, (-1, 1000, 2))
+print(traj_test.shape)
+np.save("data_test.npy", traj_test)
