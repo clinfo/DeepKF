acb0aab5b28a130f9e7fd62d177ae722126654d6
Hiroki Tanaka <rokitaniplus1@gmail.com>
2020-10-22 14:38:27 +0900

    modified dataset

--- adw_gen/dataset/make_data_adw.py
+++ adw_gen/dataset/make_data_adw.py
@@ -15,9 +15,14 @@ def make_data_train():
         print(data_train_npy,file=f)
 
 def make_data_test():
-    data_test_npy = np.array([np.concatenate(
-        [np.expand_dims(data_generator.get_asymmetric_double_well_data(99),1),
-        np.array([[0.0]] * 900)]) for i in range(500)])
+#    data_test_npy = np.array([np.concatenate(
+#        [np.expand_dims(data_generator.get_asymmetric_double_well_data(99),1),
+#        np.array([[0.0]] * 900)]) for i in range(500)])
+    datapoints = int(500)
+    traj = data_generator.get_asymmetric_double_well_data(datapoints)
+    data_test_npy = np.expand_dims(traj[0:500], 1)
+    data_test_npy = np.append(data_test_npy,[0]*100)
+    data_test_npy = np.reshape(data_test_npy, (-1, 100, 1))    
     np.save('data_test.npy', data_test_npy)
     print("data_test")
     print(data_test_npy.shape)
@@ -28,7 +33,7 @@ def make_data_test():
 
 
 def make_mask():
-    mask_test_npy = np.array([[[1]]*100+[[0]]*900]*500)
+    mask_test_npy = np.array([[[1]]*100]*5+[[[0]]*100])   
     np.save('mask_test.npy', mask_test_npy)
     print("mask_test")
     print(mask_test_npy.shape)
@@ -36,6 +41,7 @@ def make_mask():
         np.set_printoptions(threshold=np.inf)
         print(mask_test_npy,file=f)
 
+np.random.seed(0)
 make_data_train()
 make_data_test()
 make_mask()
