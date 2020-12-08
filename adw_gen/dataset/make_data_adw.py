4fe310c65b6ffe087ac7ac01c3a651b0f3c3ad0f
Hiroki Tanaka <tanaka.hiroki.82c@st.kyoto-u.ac.jp>
2020-12-08 19:13:28 +0900

    modified for generation

--- adw_gen/dataset/make_data_adw.py
+++ adw_gen/dataset/make_data_adw.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import data_generator
 
 def make_data_train():
-    datapoints = int(100000)
+    datapoints = int(49999)
     traj = data_generator.get_asymmetric_double_well_data(datapoints)
-    data_train_npy = np.expand_dims(traj[0:50000], 1)
+    data_train_npy = np.expand_dims(traj, 1)
     data_train_npy = np.reshape(data_train_npy, (-1, 100, 1))
     np.save('data_train.npy', data_train_npy)
     print("data_train")
@@ -15,13 +15,9 @@ def make_data_train():
         print(data_train_npy,file=f)
 
 def make_data_test():
-#    data_test_npy = np.array([np.concatenate(
-#        [np.expand_dims(data_generator.get_asymmetric_double_well_data(99),1),
-#        np.array([[0.0]] * 900)]) for i in range(500)])
-    datapoints = int(500)
+    datapoints = int(49999)
     traj = data_generator.get_asymmetric_double_well_data(datapoints)
-    data_test_npy = np.expand_dims(traj[0:500], 1)
-    data_test_npy = np.append(data_test_npy,[0]*100)
+    data_test_npy = np.expand_dims(traj, 1)
     data_test_npy = np.reshape(data_test_npy, (-1, 100, 1))    
     np.save('data_test.npy', data_test_npy)
     print("data_test")
@@ -31,17 +27,26 @@ def make_data_test():
         print(data_test_npy,file=f)
 
 
-
-def make_mask():
-    mask_test_npy = np.array([[[1]]*100]*5+[[[0]]*100])   
-    np.save('mask_test.npy', mask_test_npy)
-    print("mask_test")
-    print(mask_test_npy.shape)
-    with open('mask_test.txt', 'w') as f:
+def make_zeros_gen():
+    zeros_gen_npy = np.array([[[0]]*100]*5)   
+    np.save('zeros_gen.npy', zeros_gen_npy)
+    print("zeros_gen")
+    print(zeros_gen_npy.shape)
+    with open('zeros_gen.txt', 'w') as f:
         np.set_printoptions(threshold=np.inf)
-        print(mask_test_npy,file=f)
+        print(zeros_gen_npy,file=f)
+
+def make_mask_gen():
+    mask_gen_npy = np.array([[[0]]*100]*5)   
+    np.save('mask_gen.npy', mask_gen_npy)
+    print("mask_gen")
+    print(mask_gen_npy.shape)
+    with open('mask_gen.txt', 'w') as f:
+        np.set_printoptions(threshold=np.inf)
+        print(mask_gen_npy,file=f)
 
 np.random.seed(0)
 make_data_train()
 make_data_test()
-make_mask()
+make_zeros_gen()
+make_mask_gen()
