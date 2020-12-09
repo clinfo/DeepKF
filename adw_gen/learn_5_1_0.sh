209ef3620137045d3b366b0671cbb847a077b160
Hiroki Tanaka <rokitaniplus1@gmail.com>
2020-12-10 00:21:38 +0900

    adw learn (failed)

--- adw_gen/learn_5_1_0.sh
+++
@@ -1,4 +0,0 @@
-dmm train,test --config config_train_and_test.json --hyperparam hyparam/5_1_0.json --save config_train_and_test.result.json --gpu 0
-dmm-plot infer --config config_train_and_test.results.json
-python ../script/plot_loss.py results/result_5_1/ex0/log.txt results/result_5_1/ex0/plot/loss.png
-cp results/result_5_1/ex0/log.txt results/result_5_1/ex0/learn_log.txt
