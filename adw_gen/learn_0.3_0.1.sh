3f9c31c753094eaf123e87bd9d27417a8f5f8940
Hiroki Tanaka <rokitaniplus1@gmail.com>
2020-12-10 00:21:38 +0900

    adw learn (failed)

---
+++ adw_gen/learn_0.3_0.1.sh
@@ -0,0 +1,11 @@
+./learn.sh 0.3 0.1 10 &
+./learn.sh 0.3 0.1 11 &
+./learn.sh 0.3 0.1 12 &
+./learn.sh 0.3 0.1 13 &
+./learn.sh 0.3 0.1 14 &
+./learn.sh 0.3 0.1 15 &
+./learn.sh 0.3 0.1 16 &
+./learn.sh 0.3 0.1 17 & 
+./learn.sh 0.3 0.1 18 &
+./learn.sh 0.3 0.1 19 &
+
