00d819614a0dc140b781d172c99a5cb73172ca58
Hiroki Tanaka <rokitaniplus1@gmail.com>
2020-12-11 05:00:17 +0900

    generation 0.3 0.1 4

--- adw_gen/generate.sh
+++ adw_gen/generate.sh
@@ -1,2 +1,5 @@
-dmm filter --config config_generate.json --hyperparam hyparam_adw_gen_5_1_0.json
-python ../script/plot_filter.py results/result_5_1/ex0/filter.jbl results/result_5_1/ex0/plot/filter.gif
+alpha=$1
+beta=$2
+seed=$3
+dmm filter --config config_generate.json --hyperparam hyperparams/${alpha}_${beta}_${seed}.json
+python ../script/plot_filter.py results/${alpha}_${beta}_${seed}/filter.jbl results/${alpha}_${beta}_${seed}/generate.png 
