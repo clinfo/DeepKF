d207493b58ccabac5885f204a9d23ddeefc8b7e7
Hiroki Tanaka <rokitaniplus1@gmail.com>
2020-12-11 04:34:53 +0900

    analyze loss

--- adw_gen/learn.sh
+++ adw_gen/learn.sh
@@ -1,9 +1,9 @@
 alpha=${1}
 beta=${2}
 seed=${3}
-mkdir -p results/${alpha}_${beta}_${seed}/
-dmm train,test --config config_learn.json --hyperparam hyperparams/${alpha}_${beta}_${seed}.json\
- --save-config config_results/${alpha}_${beta}_${seed}.json --seed ${seed} --gpu $((seed % 3 + 1))
-dmm-plot infer --config config_results/${alpha}_${beta}_${seed}.json --limit_all 5
-python ../script/plot_loss.py results/${alpha}_${beta}_${seed}/log.txt results/${alpha}_${beta}_${seed}/plot/loss.png
-cp results/${alpha}_${beta}_${seed}/log.txt results/${alpha}_${beta}_${seed}/learn_log.txt
+#mkdir -p results/${alpha}_${beta}_${seed}/
+#dmm train,test --config config_learn.json --hyperparam hyperparams/${alpha}_${beta}_${seed}.json\
+# --save-config config_results/${alpha}_${beta}_${seed}.json --seed ${seed} --gpu $((seed % 3 + 1))
+#dmm-plot infer --config config_results/${alpha}_${beta}_${seed}.json --limit_all 5
+#python ../script/plot_loss.py results/${alpha}_${beta}_${seed}/log.txt results/${alpha}_${beta}_${seed}/plot/loss.png
+cp results/${alpha}_${beta}_${seed}/log.txt results/${alpha}_${beta}_${seed}/learn_log.tsv
